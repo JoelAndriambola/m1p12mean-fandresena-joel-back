25ca54c1812cbdec573df8c4c89b69909ac4374c
Fandry07 <fandresena2902@gmail.com>
2025-04-03 14:01:38 +0300

    make piece nullable

--- models/Intervention.js
+++ models/Intervention.js
@@ -1,26 +1,30 @@
 const mongoose = require("mongoose");
 
 const interventionSchema = new mongoose.Schema({
-  mec_id: { type: mongoose.Schema.Types.ObjectId, ref: "Utilisateur" }, // ID du mécanicien
-  rdv_id: { type: mongoose.Schema.Types.ObjectId, ref: "RendezVous" }, // ID du rendez-vous
+  mec_id: { type: mongoose.Schema.Types.ObjectId, ref: "Utilisateur", required: false }, // ID du mécanicien
+  rdv_id: { type: mongoose.Schema.Types.ObjectId, ref: "RendezVous", required: false }, // ID du rendez-vous
 
   etat: {
     type: String,
     enum: ["Assigné", "En cours", "Terminé"],
     required: true,
   },
-  date_debut: { type: Date, required: true },
-  date_fin: { type: Date, required: true },
+  date_debut: { type: Date, required: false },
+  date_fin: { type: Date, required: false },
 
   // Tableau des services avec les pièces utilisées
   // select by id service_id puis insert ou update dans intervention avec les nb piece utilisé
   services: [
     {
-      service_id: { type: mongoose.Schema.Types.ObjectId, ref: "Service" }, // Référence au modèle Service
+      service_id: { type: mongoose.Schema.Types.ObjectId, ref: "Service", required: false }, // Référence au modèle Service
       pieces_utilisees: [
         {
-          piece_id: { type: mongoose.Schema.Types.ObjectId, ref: "PieceDetachee" }, // Référence à une pièce détachée
-          quantite_utilisee: { type: Number, required: true }, // Quantité utilisée de cette pièce
+          piece_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "PieceDetachee",
+            required: false,
+          }, // Référence à une pièce détachée
+          quantite_utilisee: { type: Number, required: false }, // Quantité utilisée de cette pièce
         },
       ],
     },
